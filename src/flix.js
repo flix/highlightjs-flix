1bc6032391c526cef2162ffe6d2f5914e489caf0
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-10-03 21:49:49 +0200

    WIP: Add highlighting for built-in functions and effects

--- src/flix.js
+++ src/flix.js
@@ -267,6 +267,18 @@ export default function(hljs) {
           ],
           relevance: 0
         },
+        {
+          begin: hljs.IDENT_RE + '::',
+          keywords: {
+            keyword: "Self",
+            built_in: BUILTINS,
+            type: TYPES
+          }
+        },
+        {
+          className: "punctuation",
+          begin: '->'
+        }
       ]
     };
   }
