d190826d05c265f8f231f636fb924d3c35a203ab
Jakob Schneider <60233376+jaschdoc@users.noreply.github.com>
2022-10-15 18:02:59 +0200

    feat: add support for `debug` (#26)

--- src/flix.js
+++ src/flix.js
@@ -21,7 +21,7 @@ export default function(hljs) {
 
     const INTERPOLATION = {
       scope: 'subst',
-      begin: /\$\{/,
+      begin: /(\$|\%)\{/,
       end: /\}/
     };
 
@@ -168,6 +168,9 @@ export default function(hljs) {
     ];
 
     const BUILTIN = [
+      "debug",
+      "debug!",
+      "debug!!",
       "IO",
       "ef",
       "ef1",
